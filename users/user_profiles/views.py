70e728af9c21b8d5af54a1a92355d582c338d71d
Jyoti Gangadhar Amage <jyotia@cybage.com>
2018-04-13 13:05:02 +0530

    Chnages button to href

--- users/user_profiles/views.py
+++ users/user_profiles/views.py
@@ -17,42 +17,38 @@ def signup_view(request):
     title="Signup"
     print(request.method)
     if request.method == 'POST':
-        form = SignUpForm(data=request.POST)   
-        print(form.is_valid())      
+        form = SignUpForm(data=request.POST)
         if form.is_valid():
+            username = form.cleaned_data.get('username')
+            password = form.cleaned_data.get('password1')
             user = User.objects.create_user(username = form.cleaned_data.get('username'),
-            password = form.cleaned_data.get('password1'), email=form.cleaned_data.get('email'))
+            password = form.cleaned_data.get('password1'), email=form.cleaned_data.get('email'),
+            is_staff=form.cleaned_data.get('is_staff'), is_superuser=form.cleaned_data.get('is_superuser'))
             user.save()
-            return HttpResponseRedirect("/user/login")
+            user= authenticate(username=username, password=password)
+            dj_login(request, user)
+            return show_home(request,user)
     form = SignUpForm()
     return render(request, 'signup.html',{'form':form,"title":title})
 
-
+def show_home(request,user):
+    try:
+        if user.is_staff and user.is_superuser and user.is_active:
+            return HttpResponseRedirect("/user/"+request.user.username+"/home")
+        return HttpResponseRedirect("/user/user_profile/"+request.user.username)
+    except  RelatedObjectDoesNotExist:
+        return HttpResponseRedirect("/user/edit_profile/"+request.user.username)
 
 def login_view(request):
-    print(request.user.is_authenticated)
-    next=request.GET.get('next')
     title ="Login"
     form = UserLoginForm(request.POST or None)
 
     if form.is_valid():
         username = form.cleaned_data.get("username")
         password = form.cleaned_data.get("password")
-
         user= authenticate(username=username, password=password)
         dj_login(request, user)
-        if next:
-            return HttpResponseRedirect(next)
-        
-        try:
-            print(user.is_staff, user.is_superuser, user.is_active)
-            if user.is_staff and user.is_superuser and user.is_active:
-                return HttpResponseRedirect("/user/"+request.user.username+"/home")                    
-            return HttpResponseRedirect("/user/user_profile/"+request.user.username)
-        except  RelatedObjectDoesNotExist:
-            return HttpResponseRedirect("/user/edit_profile/"+request.user.username)
-
-        print(request.user.is_authenticated)
+        return show_home(request,user)
     return render(request,"login.html",{"form":form,"title":title})
 
 @login_required(login_url="/user/login")
@@ -60,51 +56,49 @@ def logout_view(request,username):
     logout(request)
     return HttpResponseRedirect("/user/login")
 
+def get_users(request):
+    users= User.objects.all()
+    return render(request,"error.html", {"users":users})
+
 @login_required(login_url="/user/login")
 def changePassword_view(request, username):
-    print("Inside view")
     title="Change Password"
-    print(request.POST)
     if request.method == 'POST':
         form = ChangePasswordForm(data=request.POST)
         if form.is_valid():
             user=User.objects.get(username=username)
-            print("Inside password check ")
+            if user.is_staff or user.is_superuser:
+                return get_users(request)
+
             user.set_password(form.cleaned_data['password1'])
             user.save()
             update_session_auth_hash(request, user)  # Important!
             messages.success(request, 'Your password was successfully updated!')
-            return HttpResponseRedirect("/user/user_profile/"+request.user.username)
+            return HttpResponseRedirect("/user/user_profile/"+user.username)
         else:
             messages.error(request, 'Please correct the error below.')
     else:
-        print("Inside else")
-        print(request.GET)
         if "cancel" in request.GET:
             return HttpResponseRedirect("/user/user_profile/"+request.user.username)
         form = ChangePasswordForm()
     return render(request, 'change_password.html', {
-        'form': form,'title':title  
+        'form': form,'title':title
     })
-    
+
 
 @login_required(login_url="/user/login")
 def admin_view(request,username):
-    print(username, request.user.username)
     users= User.objects.all()
-    print("Insode admin view")
     return render(request, "admin.html",{"users":users})
 
 @login_required(login_url="/user/login")
 def user_profile(request, username):
-    print("INside user profile viw")
     user = User.objects.get(username=username)
-    print(dir(user))
     return render(request, "profile_user.html", {"user":user})
 
 @login_required(login_url="/user/login")
 def update_profile(request, username):
-    if request.method == 'POST':        
+    if request.method == 'POST':
         user=User.objects.get(username=username)
         if "cancel" in request.POST:
             if user.is_staff or user.is_superuser:
@@ -112,7 +106,7 @@ def update_profile(request, username):
             return  HttpResponseRedirect("/user/user_profile/"+user.username)
         user_form = UserForm(request.POST, instance=user)
         profile_form = ProfileForm(request.POST,  request.FILES, instance=user.profile)
-        print(request.FILES)
+
         if user_form.is_valid() and profile_form.is_valid():
             print("Inside form valid")
             user_form.save(commit=True)
@@ -120,77 +114,32 @@ def update_profile(request, username):
             messages.success(request, 'Your profile was successfully updated!')
             return  HttpResponseRedirect("/user/user_profile/"+user.username)
         else:
-            messages.error(request, 'Please correct the error below.')       
+            messages.error(request, 'Please correct the error below.')
     else:
-        print(username)
-        if "cancel" in request.GET and (request.user.is_superuser or request.user.is_staff):    
-            print("Inside get if ")
+
+        if "cancel" in request.GET and (request.user.is_superuser or request.user.is_staff):
             return  HttpResponseRedirect("/user/"+request.user.username+"/home")
 
         elif "cancel" in request.GET:
             return  HttpResponseRedirect("/user/user_profile/"+request.user.username)
 
-        elif "edit" in request.GET and (request.user.is_superuser or request.user.is_staff):   
+        elif "edit" in request.GET and (request.user.is_superuser or request.user.is_staff):
             user=User.objects.get(username=username)
             if (user.is_staff or user.is_superuser) and user.username!=request.user.username:
-                users= User.objects.all()
-                print("Inside admin view")
-                return render(request,"error.html", {"users":users})
-
+                return get_users(request)
             user_form = UserForm(instance=user)
             profile_form = ProfileForm(instance=user.profile)
             request.user.username=user.username
             return render(request, 'profile.html', {
                 'user_form': user_form,
                 'profile_form': profile_form,
-
-            })    
+            })
         else:
-            print("Inside else")
+
             user_form = UserForm(instance=request.user)
             profile_form = ProfileForm(instance=request.user.profile)
-            print("No if executed")
-            print(request.user.username)
             return render(request, 'profile.html', {
                 'user_form': user_form,
                 'profile_form': profile_form,
 
-            })    
-
-
-def base_view(request):
-    return render(request,'base.html')
-
-
-
-####################Code Need to have look into it##########################
-@login_required(login_url="/user/login")
-def user_view(request, username):
-    user = User.objects.get(username=username)
-    print("Inside user view")
-    return render(request, "profile_user.html", {"user":user})
-    #return render(request, "users.html", {"user":user})
-
-@login_required(login_url="/user/login")
-def edit_profile(request,username):
-    print("Inside edit profile view")
-    user=User.objects.get(username=username)
-    return render(request, "edit_profile.html",{"user":user})
-
-    '''if request.method == 'POST':
-        print("Inside if")
-        if "cancel" in request.POST:
-            if request.user.is_staff or request.user.is_superuser:
-                return HttpResponseRedirect("/user/"+request.user.username+"/home")
-            return  HttpResponseRedirect("/user/user_profile/"+request.user.username)
-        user_form = UserForm(request.POST, instance=request.user)
-        profile_form = ProfileForm(request.POST,  request.FILES, instance=request.user.profile)
-        if user_form.is_valid() and profile_form.is_valid():
-            #img=Profile(profile_photo=request.FILES.get('profile_photo'))
-            #img.save()
-            user_form.save(commit=True)
-            profile_form.save()
-            messages.success(request, 'Your profile was successfully updated!')
-            return  HttpResponseRedirect("/user/user_profile/"+request.user.username)
-        else:
-            messages.error(request, 'Please correct the error below.')'''    
+            })
